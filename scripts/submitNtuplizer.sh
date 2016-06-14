4168dae5cfad1e311547122cdcdb90fbd27d423a
Devin Taylor <dntaylor@wisc.edu>
2016-06-14 13:32:03 +0200

    turn off phys03

--- scripts/submitNtuplizer.sh
+++ scripts/submitNtuplizer.sh
@@ -7,4 +7,4 @@ else
 fi
 submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_MC.txt "$DATE"_"$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1
 submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_Data.txt --applyLumiMask "Collisions16" "$DATE"_"$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py runMetFilter=1
-submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_phys03.txt --inputDBS phys03 "$DATE"_"$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1
+#submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_phys03.txt --inputDBS phys03 "$DATE"_"$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1
