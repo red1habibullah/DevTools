e5513a8979a9b70ff0e25528e31ba1dbbff79fae
Devin Taylor <dntaylor@wisc.edu>
2016-12-28 18:48:24 +0100

    updated recipe for 8025

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -4,31 +4,44 @@
 pushd $CMSSW_BASE/src
 
 # IDs
-git cms-merge-topic ikrav:egm_id_80X_v1
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/CutBasedElectronIdentificationRun2
+git cms-merge-topic ikrav:egm_id_80X_v2
+# https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedPhotonIdentificationRun2
+git cms-merge-topic ikrav:egm_id_80X_v3_photons
+
+# MVA
+pushd $CMSSW_BASE/external/$SCRAM_ARCH
+
+# https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2
+git clone https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
+pushd data/RecoEgamma/ElectronIdentification/data
+git checkout egm_id_80X_v1
+popd
+
+# https://twiki.cern.ch/twiki/bin/view/CMS/MultivariatePhotonIdentificationRun2
+git clone https://github.com/ikrav/RecoEgamma-PhotonIdentification.git data/RecoEgamma/PhotonIdentification/data
+pushd data/RecoEgamma/PhotonIdentification/data
+git checkout egm_id_80X_v1
+popd
+
+popd
+
+# EGMRegressions
+# https://twiki.cern.ch/twiki/bin/view/CMS/EGMRegression
+git cms-merge-topic rafaellopesdesa:Regression80XEgammaAnalysis
 
 # EGMSmearer
-## broken recipe
-#git remote add -f -t ecal_smear_fix_80X emanueledimarco https://github.com/emanueledimarco/cmssw.git
-#git cms-addpkg EgammaAnalysis/ElectronTools
-##git checkout -b from-277de3c 277de3c # old 
-#git checkout -b from-52f192a 52f192a # updated full 12.9
-# fixed recipe
-git cms-merge-topic emanueledimarco:ecal_smear_fix_80X
-
-# download the txt files with the corrections
+# https://twiki.cern.ch/twiki/bin/view/CMS/EGMSmearer
+git cms-merge-topic shervin86:Moriond2017_JEC_energyScales
 pushd EgammaAnalysis/ElectronTools/data
-# corrections calculated with the first 4/fb of 2016 data
-#git clone -b ICHEP2016_approval_4fb https://github.com/ECALELFS/ScalesSmearings.git
-# or, alternatively, corrections calculated with first 7.65/fb of 2016 data
-#git clone -b ICHEP2016_approval_7p65fb https://github.com/emanueledimarco/ScalesSmearings.git
-# corrections calculated with 12.9 fb-1 of 2016 data (ICHEP 16 dataset).
-git clone -b ICHEP2016_v2 https://github.com/ECALELFS/ScalesSmearings.git
+git clone git@github.com:ECALELFS/ScalesSmearings.git
 popd
 
 # MET
-git cms-merge-topic -u cms-met:CMSSW_8_0_X-METFilterUpdate
-#echo /PhysicsTools/PatUtils/ >> .git/info/sparse-checkout
-#git cms-merge-topic cms-met:metTool80X
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2
+git cms-merge-topic -u cms-met:fromCMSSW_8_0_20_postICHEPfilter
+# https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription
+git cms-merge-topic cms-met:METRecipe_8020
 
 popd
 
