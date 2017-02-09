c3f756162a93b79d80be0d102dc4c9c244e17322
Devin Taylor <dntaylor@wisc.edu>
2017-02-09 15:21:34 -0600

    updated for 8026

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -10,26 +10,29 @@ git cms-merge-topic gpetruc:badMuonFilters_80X_v2
 # https://twiki.cern.ch/twiki/bin/view/CMS/HEEPElectronIdentificationRun2
 git cms-merge-topic Sam-Harper:HEEPV70VID_8010_ReducedCheckout
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/CutBasedElectronIdentificationRun2
-git cms-merge-topic ikrav:egm_id_80X_v2
+# integrated in 8_0_26
+#git cms-merge-topic ikrav:egm_id_80X_v2
 # https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedPhotonIdentificationRun2
 git cms-merge-topic ikrav:egm_id_80X_v3_photons
 
-# MVA
-pushd $CMSSW_BASE/external/$SCRAM_ARCH
-
-# https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2
-git clone https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
-pushd data/RecoEgamma/ElectronIdentification/data
-git checkout egm_id_80X_v1
-popd
-
-# https://twiki.cern.ch/twiki/bin/view/CMS/MultivariatePhotonIdentificationRun2
-git clone https://github.com/ikrav/RecoEgamma-PhotonIdentification.git data/RecoEgamma/PhotonIdentification/data
-pushd data/RecoEgamma/PhotonIdentification/data
-git checkout egm_id_80X_v1
-popd
-
-popd
+# no longer needed since 8_0_25
+## MVA
+#pushd $CMSSW_BASE/external/$SCRAM_ARCH
+#
+## https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2
+## integrated in 8_0_25
+#git clone https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
+#pushd data/RecoEgamma/ElectronIdentification/data
+#git checkout egm_id_80X_v1
+#popd
+#
+## https://twiki.cern.ch/twiki/bin/view/CMS/MultivariatePhotonIdentificationRun2
+#git clone https://github.com/ikrav/RecoEgamma-PhotonIdentification.git data/RecoEgamma/PhotonIdentification/data
+#pushd data/RecoEgamma/PhotonIdentification/data
+#git checkout egm_id_80X_v1
+#popd
+#
+#popd
 
 # EGMRegressions
 # https://twiki.cern.ch/twiki/bin/view/CMS/EGMRegression
