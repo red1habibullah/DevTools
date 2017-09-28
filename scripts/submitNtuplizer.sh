81e27b96ffc0977d18564d59e2a3ee1365891ab8
Devin Taylor <dntaylor@ucdavis.edu>
2017-09-28 12:46:52 -0500

    cleanup subission script

--- scripts/submitNtuplizer.sh
+++ scripts/submitNtuplizer.sh
@@ -7,5 +7,4 @@ else
 fi
 submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_Data.txt --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=0 crab=1
 submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_MC.txt "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1
-#submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_MC_nonvalid.txt --allowNonValid "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1
 submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_phys03.txt --inputDBS phys03 "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1
