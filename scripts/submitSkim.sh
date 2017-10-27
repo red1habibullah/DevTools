b9a99c5c31f08de2ea021f2716677488bf7015dc
Devin Taylor <dntaylor@ucdavis.edu>
2017-10-27 16:02:55 -0500

    updated submission params

--- scripts/submitSkim.sh
+++ scripts/submitSkim.sh
@@ -8,18 +8,15 @@ fi
 while read sample; do
     echo $sample
     if [[ $sample == *"Run2016H"* ]]; then
-        #submit_job.py crabSubmit --publish --filesPerJob 6 --numCores 4 --maxMemoryMB 8000 --samples $sample --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=4 runH=1
-        submit_job.py crabSubmit --publish --filesPerJob 3 --numCores 2 --maxMemoryMB 4000 --samples $sample --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=2 runH=1
-        #submit_job.py crabSubmit --publish --filesPerJob 2 --samples $sample --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=1 runH=1
+        #submit_job.py crabSubmit --filesPerJob 6 --numCores 4 --maxMemoryMB 8000 --samples $sample --publish --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=4 runH=1
+        submit_job.py crabSubmit --filesPerJob 2 --samples $sample --publish --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=1 runH=1
     else
-        #submit_job.py crabSubmit --publish --filesPerJob 6 --numCores 4 --maxMemoryMB 8000 --samples $sample --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=4 runH=0
-        submit_job.py crabSubmit --publish --filesPerJob 3 --numCores 2 --maxMemoryMB 4000 --samples $sample --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=2 runH=0
-        #submit_job.py crabSubmit --publish --filesPerJob 2 --samples $sample --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=1 runH=0
+        #submit_job.py crabSubmit --filesPerJob 6 --numCores 4 --maxMemoryMB 8000 --samples $sample --publish --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=4 runH=0
+        submit_job.py crabSubmit --filesPerJob 2 --samples $sample --publish --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=0 crab=1 numThreads=1 runH=0
     fi
 done <DevTools/Ntuplizer/data/datasetList_Data_AOD.txt
 while read sample; do
     echo $sample
-    #submit_job.py crabSubmit --publish --filesPerJob 6 --numCores 4 --maxMemoryMB 8000 --samples $sample  "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=1 crab=1 numThreads=4
-    submit_job.py crabSubmit --publish --filesPerJob 3 --numCores 2 --maxMemoryMB 4000 --samples $sample  "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=1 crab=1 numThreads=2
-    #submit_job.py crabSubmit --publish --filesPerJob 2 --samples $sample  "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=1 crab=1 numThreads=1
-done <DevTools/Ntuplize/data/datasetList_MC_AOD.txt
+    #submit_job.py crabSubmit --filesPerJob 6 --numCores 4 --maxMemoryMB 8000 --samples $sample --publish "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=1 crab=1 numThreads=4
+    submit_job.py crabSubmit --filesPerJob 2 --samples $sample --publish "$NAME" DevTools/Ntuplizer/test/MuMuTauTau_cfg.py isMC=1 crab=1 numThreads=1
+done <DevTools/Ntuplizer/data/datasetList_MC_AOD.txt
